eb9fb1462df7e5589d86842712eb2e45a3df084a
antonio-cod <67842667+antonio-cod@users.noreply.github.com>
2022-08-04 20:24:47 -0400

    projeto java spring

---
+++ spring-algalog-api/src/main/java/com/algaworks/algalog/springalgalogapi/SpringAlgalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.springalgalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlgalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlgalogApiApplication.class, args);
+	}
+
+}
---
+++ spring-algalog-api/src/test/java/com/algaworks/algalog/springalgalogapi/SpringAlgalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.springalgalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlgalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
